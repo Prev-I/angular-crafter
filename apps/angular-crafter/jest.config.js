74ec2a4c7f287ef44cdacffee149399bcc6538fe
Prev-I <claudio.previtali.88@gmail.com>
2019-01-17 11:36:51 +0100

    Created barebone structure of an NX workspace with angular frontend and nestJS backend

---
+++ apps/angular-crafter/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'angular-crafter',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/angular-crafter/'
+};
---
+++ apps/server-crafter/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'server-crafter',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/server-crafter'
+};
