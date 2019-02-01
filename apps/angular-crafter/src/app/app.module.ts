f2b4eb1e07dd024db6a789e9afe668048565b5da
Prev-I <claudio.previtali.88@gmail.com>
2019-02-01 10:55:39 +0100

    Testing material datepicker inside page

--- apps/angular-crafter/src/app/app.module.ts
+++ apps/angular-crafter/src/app/app.module.ts
@@ -5,16 +5,32 @@ import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { storeFreeze } from 'ngrx-store-freeze';
+
+import {MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, MatInputModule} from '@angular/material';
 
 @NgModule({
-	declarations: [ AppComponent ],
-	imports: [
-		BrowserModule,
-		NxModule.forRoot(),
-		RouterModule.forRoot([], { initialNavigation: 'enabled' }),
-		BrowserAnimationsModule
-	],
-	providers: [],
-	bootstrap: [ AppComponent ]
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    NxModule.forRoot(),
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, { metaReducers: !environment.production ? [storeFreeze] : [] }),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
